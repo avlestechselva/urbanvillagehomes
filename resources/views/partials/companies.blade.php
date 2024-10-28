9786363459c47cb4da341e03dfc907e43497bab8
selva <selvakumar@ecyber.com>
2024-10-28 18:17:06 +0530

    corrections

--- resources/views/partials/companies.blade.php
+++ resources/views/partials/companies.blade.php
@@ -1,18 +1,41 @@
+<style>
+    #about-us-carousel .item img {
+    width: 120px; /* Set a fixed width for all logos */
+    height: 95px; /* Set a fixed height for all logos */
+    object-fit: contain; /* This ensures the image fits within the width/height without being distorted */
+    display: block;
+    margin: 0 auto; /* Center the images */
+}
+
+    #about-us-carousel .item {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: auto; /* Adjust as needed */
+    }
+</style>
 <div class="hot logo-spinner" style="margin-bottom:-32px;">
     <div class="container">
         <div class="carousel-wrap">
             <div class="owl-carousel" id="about-us-carousel">
                
-                <div class="item"><a href="https://www.hometrack.com/uk" target="_blank"><img src="{{ asset('images/logo/hometrack.jpg') }}" class="img-responsive"></a></div>
-                <div class="item"><a href="https://www.primelocation.com" target="_blank"><img src="{{ asset('images/logo/primelocation.jpg') }}" class="img-responsive"></a></div>
-                <div class="item"><a href="https://www.rightmove.co.uk/" target="_blank"><img src="{{ asset('images/logo/rightmove.jpg') }}" class="img-responsive"></a></div>
-                <div class="item"><a href="https://www.urbanvillagehomes.com/" target="_blank"><img src="{{ asset('images/logo/urbanvillagehomes.jpg') }}" class="img-responsive" /></a></div>
-                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/tenant.jpg') }}" class="img-responsive" /></a></div>
+                <div class="item"><a href="https://www.hometrack.com/uk" target="_blank"><img src="{{ asset('images/logo/hometrackx.png') }}" class="img-responsive"></a></div>
+                <div class="item"><a href="https://www.primelocation.com" target="_blank"><img src="{{ asset('images/logo/primelocationx.png') }}" class="img-responsive"></a></div>
+                <div class="item"><a href="https://www.rightmove.co.uk/" target="_blank"><img src="{{ asset('images/logo/rightmove-colour.svg') }}" class="img-responsive"></a></div>
+            
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/Tenant.png') }}" class="img-responsive" /></a></div>
                 
-                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/arla-colour.png') }}" class="img-responsive" /></a></div>
-                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/zoopla-colour.png') }}" class="img-responsive" /></a></div>
-                <div class="item"><a href="https://www.zoopla.co.uk/" target="_blank"><img src="{{ asset('images/logo/jupix.png') }}" class="img-responsive"></a></div>
-                <div class="item"><a href="https://www.zoopla.co.uk/" target="_blank"><img src="{{ asset('images/logo/tpos.jpg') }}" class="img-responsive"></a></div>
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/arla-colour.svg') }}" class="img-responsive" /></a></div>
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/zoopla-colour.svg') }}" class="img-responsive" /></a></div>
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/jupixx.png') }}" class="img-responsive"></a></div>
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/ctsi.png') }}" class="img-responsive"></a></div>
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/ombustman.png') }}" class="img-responsive"></a></div>
+                
+        
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/safeagent-colour.svg') }}" class="img-responsive"></a></div>
+                
+                <div class="item"><a href="" target="_blank"><img src="{{ asset('images/logo/tsi-colour.svg') }}" class="img-responsive"></a></div>
+                
             </div>
         </div>
         <div class="custom-nav d-none d-lg-block">
@@ -28,4 +51,4 @@
             </span>
         </div>
     </div>
-</div
+</div>
